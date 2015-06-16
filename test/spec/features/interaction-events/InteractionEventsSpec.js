9b65ff13f853aa2cb35c10879d4387bdff6b6076
Sebastian Stamm <sebastian.stamm@camunda.com>
2015-06-16 10:44:52 +0200

    feat(interaction): add interaction events
    related to CAM-4079

---
+++ test/spec/features/interaction-events/InteractionEventsSpec.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var TestHelper = require('../../../TestHelper');
+
+/* global bootstrapTable, inject */
+
+
+var interactionEventsModule = require('../../../../lib/features/interaction-events');
+
+
+describe('features/interaction-events', function() {
+
+  beforeEach(bootstrapTable({ modules: [ interactionEventsModule ] }));
+
+  beforeEach(inject(function(sheet) {
+
+    sheet.addColumn({id: 'col1'});
+    sheet.addColumn({id: 'col2'});
+    sheet.addRow({id: 'row1'});
+
+  }));
+
+
+  describe('bootstrap', function() {
+
+    it('should bootstrap diagram with component', inject(function() {}));
+
+  });
+
+
+  describe('event emitting', function() {
+
+    it('should emit element.(click|hover|out|dblclick) events', inject(function(eventBus) {
+
+      [ 'hover', 'out', 'click', 'dblclick' ].forEach(function(type) {
+
+        //TODO: Test the events
+        eventBus.on('element.' + type, function(event) {
+          console.log(type, event);
+        });
+      });
+
+    }));
+
+  });
+
+});
---
+++ test/spec/features/modeling/EditCellSpec.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var TestHelper = require('../../../TestHelper');
+
+/* global bootstrapTable, inject */
+
+var modelingModule = require('../../../../lib/features/modeling');
+
+
+describe('features/modeling - edit cell', function() {
+
+
+  before(bootstrapTable({ modules: [ modelingModule ] }));
+
+  before(inject(function(sheet) {
+    sheet.addColumn({id: 'col'});
+    sheet.addColumn({id: 'col2'});
+    sheet.addRow({id: 'row'});
+    sheet.setCellContent({row: 'row', column: 'col', content: 'asdf'});
+  }));
+
+
+  describe('basic handling', function() {
+
+    it('should render updated view', inject(function(modeling, elementRegistry) {
+
+      // when
+      modeling.editCell('row', 'col', 'foobar');
+
+      // then
+      expect(elementRegistry.getGraphics('cell_col_row').textContent).to.eql('foobar');
+    }));
+
+
+    it('should undo', inject(function(commandStack, elementRegistry) {
+
+      // when
+      commandStack.undo();
+
+      // then
+      expect(elementRegistry.getGraphics('cell_col_row').textContent).to.eql('asdf');
+    }));
+
+    it('should redo', inject(function(commandStack, elementRegistry) {
+
+      // when
+      commandStack.redo();
+
+      // then
+      expect(elementRegistry.getGraphics('cell_col_row').textContent).to.eql('foobar');
+    }));
+
+  });
+
+});
