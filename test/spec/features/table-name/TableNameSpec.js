d5b3ef0c26a3e46a719c0c40b1299ecc1a11e596
Sebastian Stamm <sebastian.stamm@camunda.com>
2015-07-07 09:47:10 +0200

    test(table name): add tests for table name module
    related to CAM-4202

---
+++ test/spec/features/table-name/TableNameSpec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var TestHelper = require('../../../TestHelper');
+
+/* global bootstrapTable, inject, sinon */
+
+
+var tableNameModule = require('../../../../lib/features/table-name');
+
+
+describe('features/table-name', function() {
+
+  beforeEach(bootstrapTable({ modules: [ tableNameModule ] }));
+
+  describe('bootstrap', function() {
+
+    it('should bootstrap diagram with component', inject(function() {}));
+
+  });
+
+
+  describe('interface', function() {
+    it('should set the name of the table', inject(function(tableName) {
+      tableName.setName('foobar');
+      expect(tableName.getNode().textContent).to.eql('foobar');
+    }));
+
+    it('should get the name of the table', inject(function(tableName) {
+      tableName.setName('foobar');
+      expect(tableName.getName()).to.eql('foobar');
+    }));
+
+    it('name should be unaffected by transient changes', inject(function(tableName) {
+      tableName.setName('TestTable');
+      tableName.getNode().textContent = 'foobar';
+      expect(tableName.getName()).to.eql('TestTable');
+    }));
+  });
+
+});
+
