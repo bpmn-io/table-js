1a73cb0dd05ed96057065b931015998826378fcf
Sebastian Stamm <sebastian.stamm@camunda.com>
2015-11-18 14:29:28 +0100

    fix(tests): provide bind polyfill for phantomJS

--- test/helper/index.js
+++ test/helper/index.js
@@ -12,6 +12,33 @@ var Table = require('../../lib/Table'),
 
 var OPTIONS, TABLE;
 
+// bind polyfill for PhantomJS
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
+
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+          return fToBind.apply(this instanceof fNOP ? this : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    if (this.prototype) {
+      // native functions don't have a prototype
+      fNOP.prototype = this.prototype;
+    }
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
 
 /**
  * Bootstrap the table given the specified options and a number of locals (i.e. services)
