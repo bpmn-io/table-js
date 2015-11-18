212f24ad89c5425878eef945dc2e32da512b4322
Sebastian Stamm <sebastian.stamm@camunda.com>
2015-11-18 10:20:48 +0100

    impr(line-numbers): debounce recalculation of linenumbers

--- test/spec/features/line-numbers/LineNumberSpec.js
+++ test/spec/features/line-numbers/LineNumberSpec.js
@@ -25,39 +25,63 @@ describe('features/utility-column', function() {
 
     it('should bootstrap diagram with component', inject(function() {}));
 
-    it('should display line numbers', inject(function(elementRegistry) {
-      var ln1 = elementRegistry.get('cell_utilityColumn_row1');
-      var ln2 = elementRegistry.get('cell_utilityColumn_row2');
+    it('should display line numbers', function(done) {
+      inject(function(elementRegistry) {
+        var ln1 = elementRegistry.get('cell_utilityColumn_row1');
+        var ln2 = elementRegistry.get('cell_utilityColumn_row2');
 
-      expect(ln1.content).to.eql(1);
-      expect(ln2.content).to.eql(2);
-    }));
+        // wait for line numbers to be updated
+        window.setTimeout(function() {
+          expect(ln1.content).to.eql(1);
+          expect(ln2.content).to.eql(2);
+          done();
+        }, 150);
+      })();
+    });
 
   });
 
 
   describe('update', function() {
-    it('should display line number for newly added row', inject(function(sheet, elementRegistry) {
-      sheet.addRow({id:'row3'});
+    it('should display line number for newly added row', function(done) {
+      inject(function(sheet, elementRegistry) {
+        sheet.addRow({id:'row3'});
 
-      var ln = elementRegistry.get('cell_utilityColumn_row3');
+        var ln = elementRegistry.get('cell_utilityColumn_row3');
 
-      expect(ln.content).to.eql(3);
-    }));
+        // wait for line numbers to be updated
+        window.setTimeout(function() {
+          expect(ln.content).to.eql(3);
+          done();
+        }, 150);
+      })();
+    });
 
-    it('should update line number when adding row in the middle', inject(function(sheet, elementRegistry) {
-      sheet.addRow({id:'row3', previous: elementRegistry.get('row1')});
+    it('should update line number when adding row in the middle', function(done) {
+      inject(function(sheet, elementRegistry) {
+        sheet.addRow({id:'row3', previous: elementRegistry.get('row1')});
 
-      expect(elementRegistry.get('cell_utilityColumn_row1').content).to.eql(1);
-      expect(elementRegistry.get('cell_utilityColumn_row3').content).to.eql(2);
-      expect(elementRegistry.get('cell_utilityColumn_row2').content).to.eql(3);
-    }));
+        // wait for line numbers to be updated
+        window.setTimeout(function() {
+          expect(elementRegistry.get('cell_utilityColumn_row1').content).to.eql(1);
+          expect(elementRegistry.get('cell_utilityColumn_row3').content).to.eql(2);
+          expect(elementRegistry.get('cell_utilityColumn_row2').content).to.eql(3);
+          done();
+        }, 150);
+      })();
+    });
 
-    it('should update line number when removing row', inject(function(sheet, elementRegistry) {
-      sheet.removeRow(elementRegistry.get('row1'));
+    it('should update line number when removing row', function(done) {
+      inject(function(sheet, elementRegistry) {
+        sheet.removeRow(elementRegistry.get('row1'));
 
-      expect(elementRegistry.get('cell_utilityColumn_row2').content).to.eql(1);
-    }));
+        // wait for line numbers to be updated
+        window.setTimeout(function() {
+          expect(elementRegistry.get('cell_utilityColumn_row2').content).to.eql(1);
+          done();
+        }, 150);
+      })();
+    });
 
   });
 
