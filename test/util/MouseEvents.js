b2091c58c7af7000ba6ceb7796252b02e1ce4f47
Ricardo Matias <ricardo.matias@camunda.com>
2016-08-31 09:17:57 +0200

    test(MouseEvents): extend the functionality

--- test/util/MouseEvents.js
+++ test/util/MouseEvents.js
@@ -1,17 +1,36 @@
 'use strict';
 
-function performMouseEvent(type, element) {
-  var evt;
+var assign = require('lodash/object/assign');
+
+function performMouseEvent(type, element, opts) {
+  var evt, options,
+      clientX = 0,
+      clientY = 0,
+      relatedTarget = null,
+      defaults = {
+        bubbles: true,
+        cancelable: true,
+        view: window
+      };
+
   if (typeof MouseEvent !== 'function') {
     evt = document.createEvent('MouseEvents');
-    evt.initMouseEvent(type,true,true,'window',0,0,0,0,0,false,false,false,false,0,null);
+
+    if (opts) {
+      clientX = opts.clientX || 0;
+      clientY = opts.clientY || 0;
+      relatedTarget = opts.relatedTarget || null;
+    }
+
+    evt.initMouseEvent(type, true, true, 'window', 0, 0, 0, clientX, clientY,
+                       false, false, false, false, 0, relatedTarget);
   } else {
-    evt = new MouseEvent(type, {
-      bubbles: true,
-      cancelable: true,
-      view: window
-    });
+
+    options = assign(defaults, opts || {});
+
+    evt = new MouseEvent(type, options);
   }
+
   return element.dispatchEvent(evt);
 }
 
