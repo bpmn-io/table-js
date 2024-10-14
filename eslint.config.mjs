7b6b1b9cbf71be13d093c0c0af4b279cfff8e062
Philipp Fromme <philipp.fromme@camunda.com>
2024-10-14 11:50:36 +0200

    chore: migrate to ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
+
+export default [
+  ...bpmnIoPlugin.configs.browser.map((config) => {
+    return {
+      ...config,
+      files: [ 'src/**/*.js' ]
+    };
+  }),
+  ...bpmnIoPlugin.configs.jsx.map((config) => {
+    return {
+      ...config,
+      files: [ 'src/**/*.js', 'test/**/*.js' ]
+    };
+  }),
+  ...bpmnIoPlugin.configs.node.map((config) => {
+    return {
+      ...config,
+      files: [ '*.js', '*.mjs', 'test/suite.js' ]
+    };
+  }),
+  ...bpmnIoPlugin.configs.mocha.map((config) => {
+    return {
+      ...config,
+      files: [ 'test/**/*.js' ]
+    };
+  }),
+  {
+    rules: {
+      'max-len': [ 'error', { 'code': 90 } ],
+      'react/display-name': 'off',
+      'react/no-deprecated': 'off',
+      'react/jsx-key': 'off', // TODO(@barmac): reenable and fix problems
+      'react/no-unknown-property': 'off',
+    }
+  },
+  {
+    ignores: [
+      'lib'
+    ]
+  }
+];
